a74c4d9519f947c0371174bb35b12c0c09c8e20f
Aldric <aldric.jourdain@epitech.eu>
2023-05-30 14:00:07 +0200

    feat: basic ogre window

---
+++ gui/include/hello.hpp
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2023
+** basicOgreApp
+** File description:
+** hello
+*/
+
+#ifndef HELLO_HPP_
+#define HELLO_HPP_
+
+
+#endif /* !HELLO_HPP_ */
--- gui/src/main.cpp
+++ gui/src/main.cpp
@@ -1,14 +1,132 @@
 /*
 ** EPITECH PROJECT, 2023
-** Zappy
+** basicOgreApp
 ** File description:
 ** main
 */
 
-#include <iostream>
+#include <OGRE/Ogre.h>
+#include <OGRE/Bites/OgreApplicationContext.h>
 
-int main(int argc, char **argv)
+class KeyHandler : public OgreBites::InputListener
 {
-    std::cout << "UwU" << std::endl;
+    bool keyPressed(const OgreBites::KeyboardEvent& evt) override
+    {
+        if (evt.keysym.sym == OgreBites::SDLK_ESCAPE)
+        {
+            Ogre::Root::getSingleton().queueEndRendering();
+        }
+        return true;
+    }
+};
+
+Ogre::ManualObject* createCubeMesh(Ogre::String name, Ogre::String matName) {
+
+	Ogre::ManualObject* cube = new Ogre::ManualObject(name);
+	cube->begin(matName);
+
+	cube->position(0.5,-0.5,1.0);cube->normal(0.408248,-0.816497,0.408248);cube->textureCoord(1,0);
+	cube->position(-0.5,-0.5,0.0);cube->normal(-0.408248,-0.816497,-0.408248);cube->textureCoord(0,1);
+	cube->position(0.5,-0.5,0.0);cube->normal(0.666667,-0.333333,-0.666667);cube->textureCoord(1,1);
+	cube->position(-0.5,-0.5,1.0);cube->normal(-0.666667,-0.333333,0.666667);cube->textureCoord(0,0);
+	cube->position(0.5,0.5,1.0);cube->normal(0.666667,0.333333,0.666667);cube->textureCoord(1,0);
+	cube->position(-0.5,-0.5,1.0);cube->normal(-0.666667,-0.333333,0.666667);cube->textureCoord(0,1);
+	cube->position(0.5,-0.5,1.0);cube->normal(0.408248,-0.816497,0.408248);cube->textureCoord(1,1);
+	cube->position(-0.5,0.5,1.0);cube->normal(-0.408248,0.816497,0.408248);cube->textureCoord(0,0);
+	cube->position(-0.5,0.5,0.0);cube->normal(-0.666667,0.333333,-0.666667);cube->textureCoord(0,1);
+	cube->position(-0.5,-0.5,0.0);cube->normal(-0.408248,-0.816497,-0.408248);cube->textureCoord(1,1);
+	cube->position(-0.5,-0.5,1.0);cube->normal(-0.666667,-0.333333,0.666667);cube->textureCoord(1,0);
+	cube->position(0.5,-0.5,0.0);cube->normal(0.666667,-0.333333,-0.666667);cube->textureCoord(0,1);
+	cube->position(0.5,0.5,0.0);cube->normal(0.408248,0.816497,-0.408248);cube->textureCoord(1,1);
+	cube->position(0.5,-0.5,1.0);cube->normal(0.408248,-0.816497,0.408248);cube->textureCoord(0,0);
+	cube->position(0.5,-0.5,0.0);cube->normal(0.666667,-0.333333,-0.666667);cube->textureCoord(1,0);
+	cube->position(-0.5,-0.5,0.0);cube->normal(-0.408248,-0.816497,-0.408248);cube->textureCoord(0,0);
+	cube->position(-0.5,0.5,1.0);cube->normal(-0.408248,0.816497,0.408248);cube->textureCoord(1,0);
+	cube->position(0.5,0.5,0.0);cube->normal(0.408248,0.816497,-0.408248);cube->textureCoord(0,1);
+	cube->position(-0.5,0.5,0.0);cube->normal(-0.666667,0.333333,-0.666667);cube->textureCoord(1,1);
+	cube->position(0.5,0.5,1.0);cube->normal(0.666667,0.333333,0.666667);cube->textureCoord(0,0);
+
+	cube->triangle(0,1,2);		cube->triangle(3,1,0);
+	cube->triangle(4,5,6);		cube->triangle(4,7,5);
+	cube->triangle(8,9,10);		cube->triangle(10,7,8);
+	cube->triangle(4,11,12);	cube->triangle(4,13,11);
+	cube->triangle(14,8,12);	cube->triangle(14,15,8);
+	cube->triangle(16,17,18);	cube->triangle(16,19,17);
+	cube->end();
+
+	return cube;
+
+}
+
+int main(void) {
+    OgreBites::ApplicationContext ctx("Zappy");
+    ctx.initApp();
+    Ogre::Root* root = ctx.getRoot();
+    Ogre::SceneManager* scnMgr = root->createSceneManager();
+
+    // register our scene with the RTSS
+    Ogre::RTShader::ShaderGenerator* shadergen = Ogre::RTShader::ShaderGenerator::getSingletonPtr();
+    shadergen->addSceneManager(scnMgr);
+
+    // Directional light
+    // Ogre::Light* directionalLight = scnMgr->createLight("DirectionalLight");
+    // directionalLight->setType(Ogre::Light::LT_DIRECTIONAL);
+    // directionalLight->setDiffuseColour(Ogre::ColourValue(1, 0, 0));
+    // directionalLight->setSpecularColour(Ogre::ColourValue(1, 0, 0));
+    // Ogre::SceneNode* directionalLightNode = scnMgr->getRootSceneNode()->createChildSceneNode();
+    // directionalLightNode->attachObject(directionalLight);
+    // directionalLightNode->setPosition(0, 0, 15);
+    // directionalLightNode->setDirection(Ogre::Vector3(0, 0, -1));
+
+
+    // without light we would just get a black screen
+    Ogre::Light* light = scnMgr->createLight("SpotLight");
+    light->setDiffuseColour(0, 0, 1);
+    light->setSpecularColour(1, 1, 1);
+    light->setType(Ogre::Light::LT_SPOTLIGHT);
+    Ogre::SceneNode* lightNode = scnMgr->getRootSceneNode()->createChildSceneNode();
+    light->setSpotlightRange(Ogre::Degree(35), Ogre::Degree(50));
+    lightNode->attachObject(light);
+    lightNode->setPosition(0, 0, 15);
+    lightNode->setDirection(0, 0, -1);
+
+    // also need to tell where we are
+    Ogre::SceneNode* camNode = scnMgr->getRootSceneNode()->createChildSceneNode();
+    camNode->setPosition(0, 0, 15);
+    camNode->lookAt(Ogre::Vector3(0, 0, -1), Ogre::Node::TS_PARENT);
+
+    // create the camera
+    Ogre::Camera* cam = scnMgr->createCamera("myCam");
+    cam->setNearClipDistance(5); // specific to this sample
+    cam->setAutoAspectRatio(true);
+    camNode->attachObject(cam);
+
+    // and tell it to render into the main window
+    ctx.getRenderWindow()->addViewport(cam);
+
+    // finally something to render
+    Ogre::Entity* ent = scnMgr->createEntity("Sinbad.mesh");
+    Ogre::SceneNode* node = scnMgr->getRootSceneNode()->createChildSceneNode();
+    node->lookAt(Ogre::Vector3(1, 0, 1), Ogre::Node::TS_PARENT);
+    node->attachObject(ent);
+
+    Ogre::MaterialPtr myMat = Ogre::MaterialManager::getSingleton().create("myMat", "General");
+    myMat->setReceiveShadows(false);
+    myMat->getTechnique(0)->setLightingEnabled(true);
+    // myMat->getTechnique(0)->getPass(0)->setDiffuse(1,1,1,1);
+    // myMat->getTechnique(0)->getPass(0)->setAmbient(0,0,0);
+    // myMat->getTechnique(0)->getPass(0)->setSelfIllumination(0,0,0);
+
+    Ogre::SceneNode* node2 = scnMgr->getRootSceneNode()->createChildSceneNode();
+    node2->setPosition(0,0,8);
+    node2->roll(Ogre::Radian(0.785398f));
+    node2->pitch(Ogre::Radian(0.349066f));
+    node2->yaw(Ogre::Radian(0.785398f));
+    node2->attachObject(createCubeMesh("Cube", "myMat"));
+
+    KeyHandler keyHandler;
+    ctx.addInputListener(&keyHandler);
+    ctx.getRoot()->startRendering();
+    ctx.closeApp();
     return 0;
 }
