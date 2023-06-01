25e76304915a652f775603af18d8fb5c17513166
tzhengtek <tzhengtek@users.noreply.github.com>
2023-06-01 14:20:05 +0000

    deploy: 1fcb3ace270dc13aedf879b2f7863c06464cc0bf

--- search/all_0.js
+++ search/all_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['ai_0',['ai',['../classzappy__ai_1_1ai.html',1,'zappy_ai']]]
+  ['ai_0',['ai',['../classai_1_1ai.html',1,'ai.ai'],['../classzappy__ai_1_1ai.html',1,'zappy_ai.ai']]]
 ];
--- search/classes_0.js
+++ search/classes_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['ai_7',['ai',['../classzappy__ai_1_1ai.html',1,'zappy_ai']]]
+  ['ai_7',['ai',['../classai_1_1ai.html',1,'ai.ai'],['../classzappy__ai_1_1ai.html',1,'zappy_ai.ai']]]
 ];
