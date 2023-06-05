ac61271fcbca4b0f4c283cf0d9b9e59f3f287124
Axel Humeau <axel.humeau@epitech.eu>
2023-06-05 20:35:25 +0200

    docs: document Socket class

--- gui/include/Socket.hpp
+++ gui/include/Socket.hpp
@@ -18,21 +18,45 @@
 #define MAX_PORT_NUMBER 65535
 
 namespace Network {
+    /// @brief Class for Socket
     class Socket {
     public:
+        /// @brief Constructor of the socket
+        /// @param ip string representing ip address to connect to
+        /// @param port port to connect to
         Socket(std::string const &ip, uint16_t port);
+        /// @brief Constructor of the socket
+        /// @param ip number representing ip address to connect to
+        /// @param port port to connect to
         Socket(uint32_t ip, uint16_t port);
         Socket(Socket const &) = delete;
+        /// @brief Destructor of the socket, close the socket file descriptor
         ~Socket();
+        /// @brief Do a select the read and write fd of the socket
         void select();
+        /// @brief Check if the socket is ready to be read, must be called after select
+        /// @return true if the socket is ready to be read, false otherwise
         bool isReadSet() const { return FD_ISSET(fd, &readfds); }
+        /// @brief Check if the socket is ready to be write, must be called after select
+        /// @return true if the socket is ready to be write, false otherwise
         bool isWriteSet() const { return FD_ISSET(fd, &writefds); }
+        /// @brief Get a string from the buffer
+        /// @param isRead true to get string from read buffer, false to get it from write buffer. true by default
+        /// @return nullopt if no string is available, otherwise a string
         std::optional<std::string> getString(bool isRead = true);
+        /// @brief Add string to the buffer
+        /// @param str String to add to the buffer
+        /// @param isRead true to put string in the read buffer, false to put it in write buffer. true by default
         void addToBuffer(std::string const &str, bool isRead = true);
+        /// @brief Send write buffer content to the socket
         void send();
 
+        /// @brief Exception to handle a connection error
         class ConnectionException : public std::exception {
         public:
+            /// @brief Constructor of the exception
+            /// @param port the port that the program was tying to connect to
+            /// @param ip the ip that the program was tying to connect to
             ConnectionException(uint16_t port, uint32_t ip);
             const char *what() const noexcept override { return base.c_str(); }
 
