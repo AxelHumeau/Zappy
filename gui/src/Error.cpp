1f41c8aa4cb12ae6754f808892c8a542163573ca
Aldric <aldric.jourdain@epitech.eu>
2023-06-06 16:48:48 +0200

    feat: addded custom errors

---
+++ gui/src/Error.cpp
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2023
+** Error.cpp
+** File description:
+** Error
+*/
+
+#include "Error.hpp"
+
+namespace ZappyGui {
+    NotImplementedError::NotImplementedError(): message("Not implemented") {}
+    NotImplementedError::~NotImplementedError() {}
+    char const *NotImplementedError::what() const noexcept { return message.c_str(); }
+
+    IndexOutOfBoundsError::IndexOutOfBoundsError(): message("Index out of bounds") {}
+    IndexOutOfBoundsError::~IndexOutOfBoundsError() {}
+    char const *IndexOutOfBoundsError::what() const noexcept { return message.c_str(); }
+}
+
