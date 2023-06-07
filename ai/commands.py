b02ab7f2b5eb6832536c8b0dffa051d00a84ce15
Axel Humeau <axel.humeau@epitech.eu>
2023-06-07 12:39:48 +0200

    feat: Incantation command

--- ai/commands.py
+++ ai/commands.py
@@ -1,1 +1,15 @@
-def do_elevation
+from queue import Queue
+from ai import AI
+
+
+def try_elevation(ai: AI, request_queue: Queue):
+    """Try to do an elevation, add the request in the queue
+
+    Returns:
+        boolean: True if the elevation requirement are fulfilled, false otherwise
+    """
+    for el in ai.elevation[ai.level].items():
+        if el[1] < ai.inventory[el[0]]:
+            return False
+    request_queue.push("Incantation")
+    return True
