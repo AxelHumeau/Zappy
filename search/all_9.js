44e25a7476133ff6abae87e9f273e52880212b17
MathieuMarques <MathieuMarques@users.noreply.github.com>
2023-06-08 15:29:33 +0000

    deploy: 3eb900dff6e7be63e14653618b44f31f13b41009

--- search/all_9.js
+++ search/all_9.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['nb_5fslots_5fleft_31',['nb_slots_left',['../structteam.html#acd91b1458e1cc6307d96c1b7ba53830f',1,'team']]],
-  ['nop_32',['nop',['../structZappyGui_1_1nop.html',1,'ZappyGui']]],
+  ['nop_32',['Nop',['../structZappyGui_1_1Nop.html',1,'ZappyGui']]],
   ['notimplementederror_33',['NotImplementedError',['../classZappyGui_1_1NotImplementedError.html',1,'ZappyGui']]]
 ];
--- search/classes_6.js
+++ search/classes_6.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['nop_86',['nop',['../structZappyGui_1_1nop.html',1,'ZappyGui']]],
+  ['nop_86',['Nop',['../structZappyGui_1_1Nop.html',1,'ZappyGui']]],
   ['notimplementederror_87',['NotImplementedError',['../classZappyGui_1_1NotImplementedError.html',1,'ZappyGui']]]
 ];
