0e65b94704ac080a7032bad7785f134be2af7b99
Axel Humeau <axel.humeau@epitech.eu>
2023-06-23 16:47:46 +0200

    docs: add documentation for particle related fonction

--- gui/include/Particles/BroadcastParticle.hpp
+++ gui/include/Particles/BroadcastParticle.hpp
@@ -12,8 +12,13 @@
 #define TO_RAD(t) (t * M_PI / 180)
 
 namespace ZappyGui {
+    /// @brief Particle effect for broadcast
     class BroadcastParticle {
     public:
+        /// @brief Construct the object
+        /// @param sceneManager the scene manager
+        /// @param origin the point of origin
+        /// @param frequency time unit of the game
         BroadcastParticle(std::shared_ptr<Ogre::SceneManager> sceneManager, Vector3 origin, size_t frequency);
         ~BroadcastParticle() = default;
 
