c44a877e45559b4da0cca33db08b20b7716c7c49
Timothé <timothe.zheng@epitech.eu>
2023-06-25 20:04:54 +0200

    feat: add documentation for the server function

--- server/include/params.h
+++ server/include/params.h
@@ -9,9 +9,37 @@
 
 #include "server.h"
 
+/**
+ * @brief Get the server params object
+ * @param params
+ * @param nb_params
+ * @param server
+ * @return int
+ */
 int get_server_params(char **params, int nb_params, struct server *server);
+/**
+ * @brief Get the port and team name object
+ * @param params
+ * @param nb_params
+ * @param server
+ * @param index
+ * @return int
+ */
 int get_port_and_team_name(char **params, int nb_params,
     struct server *server, int *index);
+/**
+ * @brief Get the dimensions freq nb client object
+ * @param params
+ * @param nb_params
+ * @param server
+ * @param index
+ * @return int
+ */
 int get_dimensions_freq_nb_client(char **params, int nb_params,
     struct server *server, int *index);
+/**
+ * @brief Set the teams object
+ * @param server
+ * @param teams
+ */
 void set_teams(struct server *server, char **teams);
